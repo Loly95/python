d1e5f5be15413f0b871dd16907a3a6c9745f2d6d
Loly95 <451010572@stu.ut.edu.sa>
2026-01-29 18:02:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 
+#Q1:
+#This code to print Welcome sentence 
+print("Welcome to Python Programming!")
+#This code to print my name and the course 
+print("My name's Layali")
+print("This is a paython code")
+
+#Q2:
+student_name="Alice"
+age= 20
+gpa=3.75
+subjects =["Math", "Python", "Physics"]
+student_info={"id": 101, "year": 2}
+skills ={"Python", "SQL"}
+is_enrolled=True
+graduation_date=None
+print(student_name)
+print(type(student_name))
+print(age)
+print(type(age))
+print(gpa)
+print(type(gpa))
+print(subjects)
+print(type(subjects))
+print(student_info)
+print(type(student_info))
+print(skills)
+print(type(skills))
+print(is_enrolled)
+print(type(is_enrolled))
+print(graduation_date)
+print(type(graduation_date))
